9914ad7d2c0e5791eb22c63b5146814571188a1c
Handré Stolp <HanStolpo@gmail.com>
2018-01-04 17:29:32 +0200

    added dnsutils and autossh

--- common/networking/default.nix
+++ common/networking/default.nix
@@ -1,5 +1,9 @@
 { config, pkgs, ... }:
 {
+  environment.systemPackages = with pkgs;[
+    dnsutils # gives nslookup and dig (and dns server enabled through services.bind.enable)
+    autossh # keep ssh tunnels open
+  ];
   networking = {
     nameservers = ["8.8.8.8" "8.8.8.4" "192.168.1.1" "192.168.100.1"];
     extraHosts =
