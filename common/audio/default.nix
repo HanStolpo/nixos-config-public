6e4d21632707f889e9a14470356f6cd24b73ee95
Handré Stolp <HanStolpo@gmail.com>
2018-01-04 17:26:12 +0200

    Pegged pulseaudio for bluetooth speaker
    Version that correctly works with my JBL Charge 2
    bluetooth speaker's handsfree mode, that is as a
    microphone and speaker at the same time.
    
    Something changed that broke this in the unstable branch.

--- common/audio/default.nix
+++ common/audio/default.nix
@@ -1,6 +1,14 @@
 { config, pkgs, ... }:
+let pkgs-17-0-9 = import (pkgs.fetchgit
+      {
+        url= "https://github.com/nixos/nixpkgs-channels";
+        rev= "3aec59c99ff6692468a069fa8a8d6a05000fca81";
+        sha256= "1w0mrsbmpfh3kxl8xkg7jccny709dammjn1lnbnrn3xx8bndsmnp";
+        fetchSubmodules= true;
+      }) {config = {allowUnfree = true;};};
+in
 {
-  environment.systemPackages = with pkgs; [
+  environment.systemPackages = with pkgs-17-0-9; [
      pavucontrol
      pasystray
      pulseaudioFull
@@ -9,7 +17,7 @@
   hardware = {
     pulseaudio = {
       enable = true;
-      package = pkgs.pulseaudioFull;
+      package = pkgs-17-0-9.pulseaudioFull;
     };
   };
 
