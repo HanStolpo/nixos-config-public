1bc4c7ea11375a341b3c18fa916a0f5db4e0fc6f
Handré Stolp <HanStolpo@gmail.com>
2022-07-05 10:39:18 +0200

    try out stable kernel

--- hosts/handre-tuxedo-laptop/default.nix
+++ hosts/handre-tuxedo-laptop/default.nix
@@ -1,5 +1,5 @@
 { config, lib, pkgs, modulesPath, ... }:
-let kernel = pkgs.pkgs-unstable.linuxPackages_latest;
+let kernel = pkgs.linuxPackages; # pkgs.pkgs-unstable.linuxPackages_latest;
     enableSSH = false;
     dpi = 122;
 in
