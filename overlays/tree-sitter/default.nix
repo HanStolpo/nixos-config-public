35ffb76e7f30973854748a3932f126acaaa8b253
Handré Stolp <HanStolpo@gmail.com>
2023-01-05 14:49:32 +0100

    switch nvim to stable pacakges

--- overlays/tree-sitter/default.nix
+++ overlays/tree-sitter/default.nix
@@ -1,6 +1,6 @@
 final: prev:
 {
-  tree-sitter = prev.pkgs-unstable.tree-sitter.override {
+  tree-sitter = prev.tree-sitter.override {
       extraGrammars = {
         tree-sitter-d2 = prev.lib.importJSON ./tree-sitter-d2.json;
         #tree-sitter-nix = prev.lib.importJSON ./tree-sitter-nix.json;
