656d67ffd074fffe0d8dc4c143b7d50beaf10589
Handré Stolp <HanStolpo@gmail.com>
2023-08-30 10:44:57 +0200

    nix version 2.17

--- overlays/nix/default.nix
+++ overlays/nix/default.nix
@@ -1,6 +1,4 @@
 final: prev: {
-  #nix = prev.nixFlakes;
-  # inherit (final.pkgs-unstable) nix-du;
-  nix = final.pkgs-unstable.nixVersions.nix_2_16;
-  inherit (final.pkgs-unstable) nix-du nix-prefetch-scripts nix-index nixpkgs-fmt;
+  nix = prev.nixVersions.nix_2_17;
+  #inherit (final.pkgs-unstable) nix-du nix-prefetch-scripts nix-index nixpkgs-fmt;
 }
