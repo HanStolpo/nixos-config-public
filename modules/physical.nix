0f0d1c36bb3291fd969f2853e82711c29fa90c13
Handré Stolp <HanStolpo@gmail.com>
2024-01-10 07:56:12 +0100

    enable udisks2 service

--- modules/physical.nix
+++ modules/physical.nix
@@ -141,6 +141,7 @@ in
 
     # allow udisks2 to work for no privileged
     # mount using `udisksctl mount --block-device /dev/sda1`
+    services.udisks2.enable = true;
     security.polkit.enable = true;
     security.polkit.extraConfig =
       ''
