0399eb23d66da7e48e71ebdf6c6aa02b746b06e2
Handré Stolp <HanStolpo@gmail.com>
2024-06-04 18:23:32 +0200

    bump nixpkgs to 24.05

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "HanStolpo's NixOS systems flake";
 
   inputs = {
-    nixpkgs.url = github:NixOS/nixpkgs/nixos-23.11;
+    nixpkgs.url = github:NixOS/nixpkgs/nixos-24.05;
     nixpkgs-unstable.url = github:NixOS/nixpkgs/nixos-unstable;
 
     kmonad = {
